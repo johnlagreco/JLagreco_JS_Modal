c333d41ff40cefe5f47bb059359b7a5ef606ce38
John LaGreco <johnlagreco@gmail.com>
2015-12-16 22:37:15 -0500

    Added script, changed some HTML

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
-		
+		$("#butt").on("click",function(e){
+		e.preventDefault();
+		var $para = $("#para");
+		$para.attr("src", $(this).attr("src"));
+		$(".surrounding").show();
+		$(".surrounding").on("click",function(){
+			$(".surrounding").hide();
+		});
+
+	});	
 
 	
 });
